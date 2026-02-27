ffe758342280bb43cb2be5c119d500e684e3d4bc
Yaron1902 <Yaron1902@proton.me>
2026-02-27 14:25:19 +0800

    Update lock-up address

--- projects/helper/bitcoin-book/echo-eBTC.js
+++ projects/helper/bitcoin-book/echo-eBTC.js
@@ -3,5 +3,6 @@ module.exports = [
   '37E3ywPuSxTHpSpK9pHqAiyYMEGEXj583u',
   '34UnQKvkiSrh46SWisN4WFDSKFHGVKyR9J',
   'bc1pfssr4x2d6g0v7ychvzl0dl5f5xemvalmar54havgk6ydwphwxnrq3u5v7f',
-  '1EsyGikimSjFDSdaNMrfH77a5WwGxaLHoh'
+  '1EsyGikimSjFDSdaNMrfH77a5WwGxaLHoh',
+  'bc1qd459g807zre4r4nelgmvkef5ddxcv5yh04vakt'
 ]
